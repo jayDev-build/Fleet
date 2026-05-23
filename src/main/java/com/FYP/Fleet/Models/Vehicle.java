6cdea068a5e7018f7784ed041e83543a58fd6c26
unknown <jainyashit11.com>
2026-05-23 13:21:23 +0530

    Vehicle User Unique Constraint

--- src/main/java/com/FYP/Fleet/Models/Vehicle.java
+++ src/main/java/com/FYP/Fleet/Models/Vehicle.java
@@ -1,16 +1,7 @@
 package com.FYP.Fleet.Models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -20,7 +11,15 @@ import lombok.Setter;
 import java.util.List;
 import java.util.ArrayList;
 
-@Entity(name = "Vehicle")
+@Entity
+@Table(name = "Vehicle",
+    uniqueConstraints = {
+            @UniqueConstraint(
+                    name = "vehicle_number_user_constriant",
+                    columnNames = {"user_id", "number"}
+            )
+    }
+)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
